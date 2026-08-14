3796bfb1122a76504ba8b79c9d8f765d8070df0a
NoMeCompila <cabafer1@gmail.com>
2026-08-13 23:19:37 -0300

    Practico 1 P6

---
+++ Practico1/Practico1/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Practico1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            BGuardar = new Button();
+            BEliminar = new Button();
+            LApellido = new Label();
+            LNombre = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            txtMultiline = new TextBox();
+            SuspendLayout();
+            // 
+            // BGuardar
+            // 
+            BGuardar.Location = new Point(46, 154);
+            BGuardar.Name = "BGuardar";
+            BGuardar.Size = new Size(75, 23);
+            BGuardar.TabIndex = 0;
+            BGuardar.Text = "Guardar";
+            BGuardar.UseVisualStyleBackColor = true;
+            // 
+            // BEliminar
+            // 
+            BEliminar.Location = new Point(128, 154);
+            BEliminar.Name = "BEliminar";
+            BEliminar.Size = new Size(75, 23);
+            BEliminar.TabIndex = 1;
+            BEliminar.Text = "Eliminar";
+            BEliminar.UseVisualStyleBackColor = true;
+            // 
+            // LApellido
+            // 
+            LApellido.AutoSize = true;
+            LApellido.Location = new Point(46, 45);
+            LApellido.Name = "LApellido";
+            LApellido.Size = new Size(51, 15);
+            LApellido.TabIndex = 2;
+            LApellido.Text = "Apellido";
+            LApellido.Click += label1_Click;
+            // 
+            // LNombre
+            // 
+            LNombre.AutoSize = true;
+            LNombre.Location = new Point(46, 92);
+            LNombre.Name = "LNombre";
+            LNombre.Size = new Size(51, 15);
+            LNombre.TabIndex = 3;
+            LNombre.Text = "Nombre";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(103, 37);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(103, 84);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 5;
+            // 
+            // txtMultiline
+            // 
+            txtMultiline.Location = new Point(251, 37);
+            txtMultiline.Multiline = true;
+            txtMultiline.Name = "txtMultiline";
+            txtMultiline.Size = new Size(207, 140);
+            txtMultiline.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(484, 261);
+            Controls.Add(txtMultiline);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(LNombre);
+            Controls.Add(LApellido);
+            Controls.Add(BEliminar);
+            Controls.Add(BGuardar);
+            Name = "Form1";
+            Text = "Mi Primer Forms";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button BGuardar;
+        private Button BEliminar;
+        private Label LApellido;
+        private Label LNombre;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox txtMultiline;
+    }
+}
