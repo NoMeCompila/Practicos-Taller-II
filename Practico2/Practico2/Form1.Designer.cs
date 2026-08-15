51fd73cb6a0658829a6d7089873589c04bdef89a
NoMeCompila <cabafer1@gmail.com>
2026-08-15 00:19:36 -0300

    TP 2 Done!

---
+++ Practico2/Practico2/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Practico2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            LDNI = new Label();
+            LApellido = new Label();
+            LNombre = new Label();
+            LNyP = new Label();
+            LModificar = new Label();
+            TDNI = new TextBox();
+            TApellido = new TextBox();
+            TNombre = new TextBox();
+            BGuardar = new Button();
+            BEliminar = new Button();
+            SuspendLayout();
+            // 
+            // LDNI
+            // 
+            LDNI.AutoSize = true;
+            LDNI.Location = new Point(63, 90);
+            LDNI.Name = "LDNI";
+            LDNI.Size = new Size(27, 15);
+            LDNI.TabIndex = 0;
+            LDNI.Text = "DNI";
+            // 
+            // LApellido
+            // 
+            LApellido.AutoSize = true;
+            LApellido.Location = new Point(63, 134);
+            LApellido.Name = "LApellido";
+            LApellido.Size = new Size(51, 15);
+            LApellido.TabIndex = 1;
+            LApellido.Text = "Apellido";
+            // 
+            // LNombre
+            // 
+            LNombre.AutoSize = true;
+            LNombre.Location = new Point(63, 185);
+            LNombre.Name = "LNombre";
+            LNombre.Size = new Size(51, 15);
+            LNombre.TabIndex = 2;
+            LNombre.Text = "Nombre";
+            // 
+            // LNyP
+            // 
+            LNyP.AutoSize = true;
+            LNyP.Location = new Point(63, 24);
+            LNyP.Name = "LNyP";
+            LNyP.Size = new Size(110, 15);
+            LNyP.TabIndex = 3;
+            LNyP.Text = "Nombre y Apellido:";
+            // 
+            // LModificar
+            // 
+            LModificar.AutoSize = true;
+            LModificar.ForeColor = Color.Red;
+            LModificar.Location = new Point(179, 24);
+            LModificar.Name = "LModificar";
+            LModificar.Size = new Size(58, 15);
+            LModificar.TabIndex = 4;
+            LModificar.Text = "Modificar";
+            // 
+            // TDNI
+            // 
+            TDNI.Location = new Point(137, 87);
+            TDNI.Name = "TDNI";
+            TDNI.Size = new Size(100, 23);
+            TDNI.TabIndex = 5;
+            TDNI.KeyPress += TDNI_KeyPress;
+            // 
+            // TApellido
+            // 
+            TApellido.Location = new Point(137, 131);
+            TApellido.Name = "TApellido";
+            TApellido.Size = new Size(100, 23);
+            TApellido.TabIndex = 6;
+            TApellido.KeyPress += TApellido_KeyPress;
+            // 
+            // TNombre
+            // 
+            TNombre.Location = new Point(137, 182);
+            TNombre.Name = "TNombre";
+            TNombre.Size = new Size(100, 23);
+            TNombre.TabIndex = 7;
+            TNombre.KeyPress += TNombre_KeyPress;
+            // 
+            // BGuardar
+            // 
+            BGuardar.Location = new Point(63, 261);
+            BGuardar.Name = "BGuardar";
+            BGuardar.Size = new Size(75, 23);
+            BGuardar.TabIndex = 8;
+            BGuardar.Text = "Guardar";
+            BGuardar.UseVisualStyleBackColor = true;
+            BGuardar.Click += BGuardar_Click;
+            // 
+            // BEliminar
+            // 
+            BEliminar.Location = new Point(162, 261);
+            BEliminar.Name = "BEliminar";
+            BEliminar.Size = new Size(75, 23);
+            BEliminar.TabIndex = 9;
+            BEliminar.Text = "Eliminar";
+            BEliminar.UseVisualStyleBackColor = true;
+            BEliminar.Click += BEliminar_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(313, 361);
+            Controls.Add(BEliminar);
+            Controls.Add(BGuardar);
+            Controls.Add(TNombre);
+            Controls.Add(TApellido);
+            Controls.Add(TDNI);
+            Controls.Add(LModificar);
+            Controls.Add(LNyP);
+            Controls.Add(LNombre);
+            Controls.Add(LApellido);
+            Controls.Add(LDNI);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Pequeño Formulario";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label LDNI;
+        private Label LApellido;
+        private Label LNombre;
+        private Label LNyP;
+        private Label LModificar;
+        private TextBox TDNI;
+        private TextBox TApellido;
+        private TextBox TNombre;
+        private Button BGuardar;
+        private Button BEliminar;
+    }
+}
